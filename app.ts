9a2a7edb67f21dd6aea91814194b02a0ac1e2f85
mediumhust <medium.hust.lee@gmail.com>
2022-08-08 17:51:00 +0700

    config multer storage

--- app.ts
+++ app.ts
@@ -9,7 +9,19 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
 });
 
-const upload = multer({ dest: "uploads/" });
+var storage = multer.diskStorage({
+  destination: function(req, file, callback) {
+    callback(null, "./uploads");
+  },
+  filename: function(req, file, callback) {
+    const extention = file.mimetype.split("/")[1];
+    var imageUrl = file.fieldname + "-" + Date.now() + "." + extention;
+    callback(null, imageUrl);
+  },
+});
+
+const upload = multer({ storage });
+//const upload = multer({ dest: "uploads/" });
 const cpUpload = upload.fields([{ name: "csv" }, { name: "pdfDoc" }]);
 app.post("/feed", cpUpload, (req, res) => {
   console.log(req.files);
